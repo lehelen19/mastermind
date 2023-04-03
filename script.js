a77e39842e0a73eca66213eb54e44e390b7b3203
lehelen19 <le.helen19@gmail.com>
2023-04-03 14:05:01 -0700

    Create function that generates random code

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+/*----- constants -----*/
+
+/*----- state variables -----*/
+
+/*----- cached elements  -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
+function generateCode() {
+  const code = [];
+  for (let i = 0; i < 6; i++) {
+    const randInt = Math.floor(Math.random() * 6);
+    code.push(randInt);
+  }
+  return code;
+}
